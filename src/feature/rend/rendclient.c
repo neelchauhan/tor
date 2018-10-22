980f21a0303e7e8559d4960519cc5330ea04e711
Neel Chauhan <neel@neelc.org>
2018-10-22 18:56:16 -0400

    Don't close intros with isolation flags in rend_client_close_other_intros()

--- src/feature/rend/rendclient.c
+++ src/feature/rend/rendclient.c
@@ -361,10 +361,21 @@ rend_client_close_other_intros(const uint8_t *rend_pk_digest)
       origin_circuit_t *oc = TO_ORIGIN_CIRCUIT(c);
       if (oc->rend_data &&
           rend_circuit_pk_digest_eq(oc, rend_pk_digest)) {
-        log_info(LD_REND|LD_CIRC, "Closing introduction circuit %d that we "
-                 "built in parallel (Purpose %d).", oc->global_identifier,
-                 c->purpose);
-        circuit_mark_for_close(c, END_CIRC_REASON_IP_NOW_REDUNDANT);
+        int has_isolation_flag = 0;
+        for (edge_connection_t *oc_stream = oc->p_streams; oc_stream != NULL;
+             oc_stream = oc_stream->next_stream) {
+          if (EDGE_TO_ENTRY_CONN(oc_stream)->entry_cfg.isolation_flags) {
+            has_isolation_flag = 1;
+            break;
+          }
+        }
+
+        if (!has_isolation_flag) {
+          log_info(LD_REND|LD_CIRC, "Closing introduction circuit %d that we "
+                   "built in parallel (Purpose %d).", oc->global_identifier,
+                   c->purpose);
+          circuit_mark_for_close(c, END_CIRC_REASON_IP_NOW_REDUNDANT);
+        }
       }
     }
   }
